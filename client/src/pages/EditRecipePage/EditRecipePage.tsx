6f5575ef3214c0b247f184ebf8222ee1b1b39ce2
charlatane <fahimfaisal778@gmail.com>
2021-11-04 05:05:29 +0600

    Created Edit recipe pages but its same as create recipe

--- client/src/pages/EditRecipePage/EditRecipePage.tsx
+++ client/src/pages/EditRecipePage/EditRecipePage.tsx
@@ -1,8 +1,6 @@
 import React, { useEffect, useState } from "react";
-
 import { Heading, Stack } from "@chakra-ui/layout";
 import { Input, FormLabel, FormControl } from "@chakra-ui/react";
-
 import SectionDivider from "../../components/SectionDivider";
 import PageContainer from "../../components/PageContainer";
 import PageSection from "../../components/PageSection";
@@ -10,13 +8,12 @@ import { Button } from "@chakra-ui/react";
 import axios from "axios";
 import { Image } from "cloudinary-react";
 
-const CreateRecipePage = (props: any) => {
+const EditRecipePage = (props: any) => {
     const [recipeTitle, setRecipeTitle] = useState<string>("");
     const [ingredient, setIngredient] = useState<string>("");
     const [instruction, setInstruction] = useState<string>("");
 
     const [imageSelected, setImageSelected] = useState<string>("");
-    //const [images,setImages] = useState([]);
 
     const uploadImage = () => {
         //console.log(files[0]);
@@ -29,16 +26,6 @@ const CreateRecipePage = (props: any) => {
         axios.post("https://api.cloudinary.com/v1_1/charlatane2499/image/upload", formData).then((response) => {
             console.log(response);
         })
-        
-       /*fetch("https://api.cloudinary.com/v1_1/dvn255pxj/image/upload", {
-           method:'POST',
-           body:formData,
-       })
-         .then((req)=>req.json())
-         .then((req) => {
-            setImages(images => [...images,res.secure_url]);
-         })
-            .catch((error) => console.log(console.error));*/
 
     };
 
@@ -49,14 +36,14 @@ const CreateRecipePage = (props: any) => {
 
     return (
         <PageContainer variant="navbar">
-            <Heading variant="page">Create a New Recipe</Heading>
+            <Heading variant="page">Edit Your Recipe</Heading>
             <SectionDivider />
 
             <Stack px={0} mx={0} justifyContent="space-between" spacing={12}>
                 <PageSection>
                     {/* Recipe title will already be set */}
 
-                    <Heading variant="section">Recipe Title
+                    <Heading variant="section">Edit Recipe Title
                     </Heading>
                     <SectionDivider />
 
@@ -82,7 +69,7 @@ const CreateRecipePage = (props: any) => {
                 </PageSection>
 
                 <PageSection>
-                    <Heading variant="section">Image</Heading>
+                    <Heading variant="section">Edit Image</Heading>
                     <SectionDivider />
                     <Input type="file" onChange={(event) => {
                         setImageSelected(event.target.files[0]);
@@ -103,7 +90,7 @@ const CreateRecipePage = (props: any) => {
 
 
                 <PageSection>
-                    <Heading variant="section">Ingredients</Heading>
+                    <Heading variant="section">Edit Ingredients</Heading>
                     <SectionDivider />
                     <FormControl id="ingredient">
                         <Input variant="flushed"
@@ -125,7 +112,7 @@ const CreateRecipePage = (props: any) => {
 
 
                 <PageSection>
-                    <Heading variant="section">Instructions</Heading>
+                    <Heading variant="section">Edit Instructions</Heading>
                     <SectionDivider />
 
                     <FormControl id="instruction">
@@ -152,11 +139,11 @@ const CreateRecipePage = (props: any) => {
                 <Button type='submit' boxShadow='sm'
                     _hover={{ boxShadow: 'md' }}
                     _active={{ boxShadow: 'lg' }} >
-                    Submit Recipe
+                    Edit Recipe
                 </Button>
             </Stack>
         </PageContainer>
     );
 };
 
-export default CreateRecipePage;
+export default EditRecipePage;
