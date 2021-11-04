cb424c69406cf34a030f352e4abad3a00b4780c4
zedrex <adib14021999@gmail.com>
2021-11-04 06:52:18 +0600

    Fixed image uploading

--- client/src/api/config.api.ts
+++ client/src/api/config.api.ts
@@ -3,8 +3,8 @@ import axios from "axios";
 // export const baseUrl = "http://localhost:4000";
 export const baseUrl = "http://e49d-103-122-249-5.ngrok.io";
 
-axios.defaults.headers.common["Authentication"] =
-    window.localStorage.getItem("Token");
+// axios.defaults.headers.common["Authentication"] =
+//     window.localStorage.getItem("Token");
 
-axios.defaults.headers.common["Content-Type"] =
-    window.localStorage.getItem("application/json");
+// axios.defaults.headers.common["Content-Type"] =
+//     window.localStorage.getItem("application/json");
--- client/src/pages/CreateRecipePage/CreateRecipePage.tsx
+++ client/src/pages/CreateRecipePage/CreateRecipePage.tsx
@@ -1,7 +1,14 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 
 import { Heading, Stack } from "@chakra-ui/layout";
-import { Input, FormLabel, FormControl } from "@chakra-ui/react";
+import {
+    Input,
+    FormLabel,
+    FormControl,
+    Textarea,
+    Text,
+    useToast,
+} from "@chakra-ui/react";
 
 import SectionDivider from "../../components/SectionDivider";
 import PageContainer from "../../components/PageContainer";
@@ -9,28 +16,58 @@ import PageSection from "../../components/PageSection";
 import { Button } from "@chakra-ui/react";
 import axios from "axios";
 import { Image } from "cloudinary-react";
+import Loading from "../../components/Loading";
 
 const CreateRecipePage = (props: any) => {
+    const toast = useToast();
+
     const [recipeTitle, setRecipeTitle] = useState<string>("");
     const [ingredient, setIngredient] = useState<string>("");
     const [instruction, setInstruction] = useState<string>("");
+    const [imageSelected, setImageSelected] = useState("");
+    const [cloudinaryLink, setCloudinaryLink] = useState<string>("");
+    const [imageLoading, setImageLoading] = useState<boolean>(false);
 
-    const [imageSelected, setImageSelected] = useState<string>("");
     //const [images,setImages] = useState([]);
 
     const uploadImage = () => {
         //console.log(files[0]);
-        const formData = new FormData()
-        formData.append("file", imageSelected)
-        formData.append("upload_preset", "g9faugip")
-     
+        const formData = new FormData();
+        formData.append("file", imageSelected);
+        formData.append("upload_preset", "g9faugip");
+        setImageLoading(true);
+        axios
+            .post(
+                "https://api.cloudinary.com/v1_1/charlatane2499/image/upload",
+                formData
+            )
+            .then((response) => {
+                console.log(response.data);
+                setCloudinaryLink(response.data.secure_url);
+                toast({
+                    position: "top",
+                    title: "Success",
+                    description: "Image uploaded successfully.",
+                    status: "success",
+                    duration: 2000,
+                    isClosable: true,
+                });
+            })
+            .catch((error) => {
+                console.log(error);
+                toast({
+                    position: "top",
+                    title: "Error",
+                    description:
+                        "Something unexpected occurred. Please reupload",
+                    status: "error",
+                    duration: 2000,
+                    isClosable: true,
+                });
+            });
+        setImageLoading(false);
 
-        
-        axios.post("https://api.cloudinary.com/v1_1/charlatane2499/image/upload", formData).then((response) => {
-            console.log(response);
-        })
-        
-       /*fetch("https://api.cloudinary.com/v1_1/dvn255pxj/image/upload", {
+        /*fetch("https://api.cloudinary.com/v1_1/dvn255pxj/image/upload", {
            method:'POST',
            body:formData,
        })
@@ -39,13 +76,7 @@ const CreateRecipePage = (props: any) => {
             setImages(images => [...images,res.secure_url]);
          })
             .catch((error) => console.log(console.error));*/
-
     };
-
-
-    useEffect(() => {
-        console.log('render');
-    }, []);
 
     return (
         <PageContainer variant="navbar">
@@ -56,18 +87,17 @@ const CreateRecipePage = (props: any) => {
                 <PageSection>
                     {/* Recipe title will already be set */}
 
-                    <Heading variant="section">Recipe Title
-                    </Heading>
+                    <Heading variant="section">Recipe Title</Heading>
                     <SectionDivider />
 
                     <FormControl id="recipe-title">
-                        <FormLabel>
-                        </FormLabel>
-                        <Input variant="flushed"
-                            placeholder="eg.Fried Rice,Shahi Mughal Biriyani.."
+                        <FormLabel></FormLabel>
+                        <Input
+                            variant="flushed"
+                            p={2}
+                            placeholder="Chicken Fry"
                             size="lg"
                             value={recipeTitle}
-                            type="text"
                             onChange={(event) => {
                                 setRecipeTitle(event.target.value);
                             }}
@@ -78,35 +108,78 @@ const CreateRecipePage = (props: any) => {
                             focusBorderColor="orange.400"
                         />
                     </FormControl>
-
                 </PageSection>
 
                 <PageSection>
                     <Heading variant="section">Image</Heading>
                     <SectionDivider />
-                    <Input type="file" onChange={(event) => {
-                        setImageSelected(event.target.files[0]);
-                    }}  >
-
-                    </Input>
-                    <Button type="submit" onClick={uploadImage}>Upload Image</Button>
-
-                     <Image style={{ width:500,height:500 }}
-                        cloudName="charlatane2499"
-                        publicId=""
-                        //publicId="https://res.cloudinary.com/charlatane2499/image/upload/v1635979399/sample.jpg"
-                        //publicId={images.map(image => <img src = {image} alt=" " />)} 
-                        /> 
-                        
-
+                    <FormLabel
+                        for="image-upload"
+                        my={4}
+                        py={2}
+                        textAlign="center"
+                        textTransform="uppercase"
+                        fontWeight="bold"
+                        color="white"
+                        bgColor="orange.500"
+                        borderRadius="md"
+                        _hover={{
+                            backgroundColor: "orange.400",
+                            _disabled: {
+                                backgroundColor: "black",
+                            },
+                        }}
+                        _disabled={{
+                            backgroundColor: "orange.800",
+                        }}
+                        width="268px"
+                        height="10"
+                        cursor={"pointer"}
+                    >
+                        Choose File
+                    </FormLabel>
+                    <Input
+                        id="image-upload"
+                        display="none"
+                        variant="unstyled"
+                        type="file"
+                        onChange={(event) => {
+                            setImageSelected(event.target.files[0]);
+                            toast({
+                                position: "top",
+                                title: "Success",
+                                description:
+                                    "Image has been selected. Please upload it.",
+                                status: "success",
+                                duration: 2000,
+                                isClosable: true,
+                            });
+                        }}
+                    />
+                    {cloudinaryLink !== "" ? (
+                        <Image
+                            w={{ base: "100%", lg: "50%" }}
+                            src={cloudinaryLink}
+                        />
+                    ) : (
+                        <Text>Choose an image and upload it.</Text>
+                    )}
+                    {imageLoading === true ? <Loading /> : <></>}
+                    <Button
+                        disabled={imageSelected === ""}
+                        onClick={uploadImage}
+                    >
+                        Upload Image
+                    </Button>
                 </PageSection>
-
 
                 <PageSection>
                     <Heading variant="section">Ingredients</Heading>
                     <SectionDivider />
                     <FormControl id="ingredient">
-                        <Input variant="flushed"
+                        <Input
+                            variant="flushed"
+                            p={2}
                             placeholder="eg.potato,rice,egg.."
                             size="lg"
                             value={ingredient}
@@ -123,19 +196,18 @@ const CreateRecipePage = (props: any) => {
                     </FormControl>
                 </PageSection>
 
-
                 <PageSection>
                     <Heading variant="section">Instructions</Heading>
                     <SectionDivider />
 
                     <FormControl id="instruction">
-                        <FormLabel>
-                        </FormLabel>
-                        <Input variant="flushed"
+                        <FormLabel></FormLabel>
+                        <Textarea
+                            variant="outline"
                             placeholder="eg. One cup of milk with half spoon sugar ..."
-                            size="lg"
+                            size="xl"
+                            p={2}
                             value={instruction}
-                            type="text"
                             onChange={(event) => {
                                 setInstruction(event.target.value);
                             }}
@@ -146,12 +218,12 @@ const CreateRecipePage = (props: any) => {
                             focusBorderColor="orange.400"
                         />
                     </FormControl>
-
-
                 </PageSection>
-                <Button type='submit' boxShadow='sm'
-                    _hover={{ boxShadow: 'md' }}
-                    _active={{ boxShadow: 'lg' }} >
+                <Button
+                    boxShadow="sm"
+                    _hover={{ boxShadow: "md" }}
+                    _active={{ boxShadow: "lg" }}
+                >
                     Submit Recipe
                 </Button>
             </Stack>
